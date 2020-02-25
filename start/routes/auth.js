7f533e6a4d6c81263d464ce22a7aa74fdff80221
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-02-25 11:03:47 -0300

    Authentication controller/routes

---
+++ start/routes/auth.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route');
+
+Route.group(() => {
+  // Authentication routes
+  Route.post('/register', 'AuthController.register').as('auth.register');
+  Route.post('/login', 'AuthController.login').as('auth.login');
+  Route.post('/logout', 'AuthController.logout').as('auth.logout');
+  Route.post('/refresh', 'AuthController.refresh').as('auth.refresh');
+
+  // Restoure password routes
+  Route.post('/reset-password', 'AuthController.forgot').as('auth.forgot');
+  Route.get('/reset-password', 'AuthController.remember').as('auth.remember');
+  Route.put('/reset-password', 'AuthController.reset').as('auth.reset');
+})
+  .prefix('v1/auth')
+  .namespace('Auth');
