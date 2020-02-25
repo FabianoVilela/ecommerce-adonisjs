158c4d7c9473a42c70b544e62d98611ca5b132e5
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-02-25 13:42:19 -0300

    User register

--- app/Controllers/Http/AuthController/Auth.js
+++ app/Controllers/Http/AuthController/Auth.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const Database = use('Database');
+const User = use('User');
+const User = use('Role');
+
 class AuthController {
   async register({req, res}) {
-    //
+    const trx = await Database.beginTransaction();
+
+    try {
+      const {name, surname, email, passoword} = Request.call();
+      const user = await User.create({name, surname, email, passoword}, trx);
+      const userRole = await userRole.findBy('slug', 'customer');
+      await user.roles().attach([userRole.id], null, trx);
+
+      await trx.commit();
+
+      return Response.status(201).send({data: user});
+    } catch (error) {
+      await trx.rollBack();
+      return Response.status(400).send({
+        message: 'User register error',
+      });
+    }
   }
 
   async login({req, res, auth}) {
