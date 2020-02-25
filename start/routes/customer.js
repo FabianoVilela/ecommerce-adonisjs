afc1f57ac4c8f6800893646fdc826dd5e9c8f1f8
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-02-25 13:26:30 -0300

    Controllers and routes

---
+++ start/routes/customer.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route');
+
+Route.group(() => {
+  // Products
+  Route.get('products', 'ProductController.index');
+  Route.get('products/:id', 'ProductController.show');
+
+  // Orders
+  Route.get('orders', 'OrderController.index');
+  Route.get('orders/:id', 'OrderController.show');
+  Route.post('orders', 'OrderController.store');
+  Route.put('orders/:id', 'OrderController.put');
+})
+  .prefix('v1')
+  .namespace('Customer');
