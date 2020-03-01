74f2162aebd7c1ebb7c079e288eefb95feb6a779
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-03-01 12:42:56 -0300

    Route orders discount

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -9,6 +9,10 @@ Route.group(() => {
   Route.resource('images', 'ImageController').apiOnly();
   Route.resource('users', 'UserController').apiOnly();
   Route.resource('coupons', 'CouponController').apiOnly();
+
+  // Orders
+  Route.post('orders', '/:id/discount', 'OrderController.applyDiscount');
+  Route.delete('orders', '/:id/discount', 'OrderController.removeDiscount');
   Route.resource('orders', 'OrderController').apiOnly();
 })
   .prefix('v1/admin')
